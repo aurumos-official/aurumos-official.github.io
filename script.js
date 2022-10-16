a14bdfeb9c8cf1cfe99aa4ee16c64f114557ab44
AndrewKitaev <creed_kitaev@outlook.com>
2022-10-16 12:58:04 +0300

    4

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function download() {
 	// in dev
 }
+
+sidebar_visible = true;
+
+function openNav() {
+	if (sidebar_visible === true) {
+		document.getElementById("older_nav").style.width = "160px";
+		document.getElementById("main").style.marginLeft = "160px";
+		sidebar_visible = false;
+	} else {
+		document.getElementById("older_nav").style.width = "0";
+		document.getElementById("main").style.marginLeft = "0";
+		sidebar_visible = true;
+	}
+    
+}
